00735cc19e52445905bf261033ff035e3aca3179
lidaguang <411905751@qq.com>
2016-03-03 20:10:45 +0800

    change send_keys too

--- mygit.py
+++ mygit.py
@@ -6,5 +6,5 @@ from selenium import webdriver
 driver=webdriver.Firefox()
 
 driver.get('http://www.baidu.com')
-driver.find_element_by_tag_name('input').send_keys('selenium')
+driver.find_element_by_tag_name('input').send_keys('wqw')
 driver.quit()
