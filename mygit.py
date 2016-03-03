c3762c83cd598d1e58567a95159f5e4d9b7d7029
lidaguang <411905751@qq.com>
2016-03-03 20:24:42 +0800

    debug fixed

--- mygit.py
+++ mygit.py
@@ -6,9 +6,5 @@ from selenium import webdriver
 driver=webdriver.Firefox()
 
 driver.get('http://www.baidu.com')
-<<<<<<< HEAD
 driver.find_element_by_tag_name('input').send_keys('wqw')
-=======
-driver.find_element_by_tag_name('input').send_keys('asas')
->>>>>>> feature1
 driver.quit()
